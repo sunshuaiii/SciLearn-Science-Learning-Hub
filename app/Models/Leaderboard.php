ab4e4b691b1e52942ab4489e8498675d8a41548d
sunshuaiii <77444967+sunshuaiii@users.noreply.github.com>
2023-04-04 10:06:01 +0800

    add models

---
+++ app/Models/Leaderboard.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Leaderboard extends Model
+{
+    use HasFactory;
+}
---
+++ app/Models/UserBadge.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserBadge extends Model
+{
+    use HasFactory;
+    public $timestamps = false;
+}
