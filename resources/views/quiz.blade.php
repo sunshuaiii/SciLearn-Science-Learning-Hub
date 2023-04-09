ce9761711abbf8edabe77c840a586f6dec1cdc51
sunshuaiii <77444967+sunshuaiii@users.noreply.github.com>
2023-04-10 02:44:47 +0800

    start quiz done

--- resources/views/quiz.blade.php
+++ resources/views/quiz.blade.php
@@ -19,45 +19,45 @@
             @foreach($questions as $question)
             <div class="quiz-card mx-auto">
                 <div class="card-body">
-                    <h5 class="card-title text-center mb-4">{{ $question['question'] }}</h5>
+                    <h5 class="card-title text-center mb-4">Question {{ $loop->iteration }} - {{ $question['question'] }}</h5>
                     <form>
                         <div class="row">
                             <div class="col-sm-6">
                                 <div class="form-check">
-                                    <input class="form-check-input" type="radio" name="answer" id="answer1" value="1">
-                                    <label class="form-check-label" for="answer1">
+                                    <input class="form-check-input" type="radio" name="answer{{ $loop->iteration }}" id="answer1{{ $loop->iteration }}" value="1">
+                                    <label class="form-check-label" for="answer1{{ $loop->iteration }}">
                                         1. {{ $question['option1'] }}
                                     </label>
                                 </div>
                             </div>
                             <div class="col-sm-6">
                                 <div class="form-check">
-                                    <input class="form-check-input" type="radio" name="answer" id="answer2" value="2">
-                                    <label class="form-check-label" for="answer2">
+                                    <input class="form-check-input" type="radio" name="answer{{ $loop->iteration }}" id="answer2{{ $loop->iteration }}" value="2">
+                                    <label class="form-check-label" for="answer2{{ $loop->iteration }}">
                                         2. {{ $question['option2'] }}
                                     </label>
                                 </div>
                             </div>
                             <div class="col-sm-6">
                                 <div class="form-check">
-                                    <input class="form-check-input" type="radio" name="answer" id="answer3" value="3">
-                                    <label class="form-check-label" for="answer3">
+                                    <input class="form-check-input" type="radio" name="answer{{ $loop->iteration }}" id="answer3{{ $loop->iteration }}" value="3">
+                                    <label class="form-check-label" for="answer3{{ $loop->iteration }}">
                                         3. {{ $question['option3'] }}
                                     </label>
                                 </div>
                             </div>
                             <div class="col-sm-6">
                                 <div class="form-check">
-                                    <input class="form-check-input" type="radio" name="answer" id="answer4" value="4">
-                                    <label class="form-check-label" for="answer4">
+                                    <input class="form-check-input" type="radio" name="answer{{ $loop->iteration }}" id="answer4{{ $loop->iteration }}" value="4">
+                                    <label class="form-check-label" for="answer4{{ $loop->iteration }}">
                                         4. {{ $question['option4'] }}
                                     </label>
                                 </div>
                             </div>
                         </div>
-                        <button type="button" class="btn btn-primary mt-3" onclick="checkAnswer('{{ json_encode($question) }}')">Submit</button>
+                        <button type="button" class="btn btn-primary mt-3" onclick="checkAnswer('{{ json_encode($question) }}', {{ $loop->iteration }})">Submit</button>
                     </form>
-                    <div id="answer-feedback" class="mt-3 d-none"></div>
+                    <div id="answer-feedback{{ $loop->iteration }}" class="mt-3 d-none"></div>
                     <br>
                 </div>
             </div>
@@ -68,29 +68,26 @@
 </div>
 
 <script>
-    function checkAnswer(question) {
+    function checkAnswer(question, questionNumber) {
         const correctAnswer = JSON.parse(question).answer;
-        console.log(correctAnswer);
-        if (document.querySelector('input[name="answer"]') === null) {
-            console.log("Please select an answer!");
+        const selectedAnswer = document.querySelector('input[name="answer' + questionNumber + '"]:checked');
+        const answerFeedback = document.getElementById("answer-feedback" + questionNumber);
 
-        } else {
-            const selectedAnswer = document.querySelector('input[name="answer"]:checked').value; //bug
-        }
-
-        const answerFeedback = document.getElementById("answer-feedback");
-        console.log(answerFeedback);
-
-        if (selectedAnswer.toString() === correctAnswer.toString()) {
-            answerFeedback.classList.remove("d-none");
-            answerFeedback.classList.add("alert", "alert-success");
-            answerFeedback.innerHTML = "You got it right!";
-        } else {
+        if (!selectedAnswer) {
             answerFeedback.classList.remove("d-none");
             answerFeedback.classList.add("alert", "alert-danger");
-            answerFeedback.innerHTML = "Incorrect. The correct answer is " + correctAnswer + ".";
+            answerFeedback.innerHTML = "Please select an answer!";
+        } else {
+            if (selectedAnswer.value.toString() === correctAnswer.toString()) {
+                answerFeedback.classList.remove("d-none", "alert-danger");
+                answerFeedback.classList.add("alert", "alert-success");
+                answerFeedback.innerHTML = "You got it right!";
+            } else {
+                answerFeedback.classList.remove("d-none", "alert-success");
+                answerFeedback.classList.add("alert", "alert-danger");
+                answerFeedback.innerHTML = "Incorrect. The correct answer is " + correctAnswer + ".";
+            }
         }
-
     }
 </script>
 
