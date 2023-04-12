1a98ace0f9a02ad88a452d0e9892cb956f00e861
Yang Chu Yan <chuyanyang01@gmail.com>
2023-04-12 10:13:34 +0800

    fix user progress UI bug

--- resources/views/student/progress.blade.php
+++ resources/views/student/progress.blade.php
@@ -7,20 +7,20 @@
 <div id="verticalScroll">
 	<div style="display:none">{{ $i=0 }}</div>
 	@foreach (App\Models\Module::take(3)->get() as $module)
-	<div style="margin: 2rem;">
-		<h2 style="text-align: center; margin: 2rem;">{{ ucwords(str_replace('-', ' ', $module->name)) }}</h2>
-		@foreach ($module->getTopics as $topic)
-		<div style="margin-bottom: 1rem;">
-			<h5>{{ $topic->name }}</h5>
-			<div class="progress" style="height: 1.5rem;">
-				<div class="progress-bar bg-success progress-bar-striped active rounded-pill" role="progressbar" aria-valuenow="{{ $progress[$i] }}" aria-valuemin="0" aria-valuemax="100" style="width: {{ $progress[$i++] * 100 }}%;">
-					{{ number_format($progress[$i - 1]*100, 2, '.', ',') }}%
+		<div style="margin: 2rem;">
+			<h2 style="text-align: center; margin: 2rem;">{{ ucwords(str_replace('-', ' ', $module->name)) }}</h2>
+			@foreach ($module->getTopics as $topic)
+				<div style="margin-bottom: 1rem;">
+					<h5>{{ $topic->name }}</h5>
+					<div class="progress" style="height: 1.5rem;">
+						<div class="progress-bar bg-success progress-bar-striped active rounded-pill" role="progressbar" aria-valuenow="{{ $progress[$i] }}" aria-valuemin="0" aria-valuemax="100" style="width: {{ $progress[$i++] * 100 }}%;">
+							{{ number_format($progress[$i - 1]*100, 2, '.', ',') }}%
+						</div>
+					</div>
 				</div>
-			</div>
 			@endforeach
 		</div>
-		@endforeach
-	</div>
+	@endforeach
 </div>
 
 <style>
